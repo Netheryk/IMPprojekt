ac47c62e2108d748377551d2e66db5985f95970c
Netheryk <jakubpluhar3@gmail.com>
2023-12-19 18:54:52 +0100

    Shooting

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -1,33 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
     Transform bulletSpawner;
-    Vector2 cursorPosition;
+    Vector3 cursorPosition;
     float angleToCursor;
     Quaternion spawnerRotation;
+    Vector3 rotation;
+    float rotZ;
+
+    [SerializeField] GameObject bulletPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        bulletSpawner = gameObject.transform;
-        spawnerRotation = bulletSpawner.transform.rotation;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        cursorPosition = Camera.main.WorldToScreenPoint(Input.mousePosition); //get position of cursor in world camera
-        angleToCursor = Mathf.Atan2(cursorPosition.y, cursorPosition.x) * Mathf.Rad2Deg; //calculate angle to cursor to rotate bulletSpawner
+        cursorPosition = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().WorldToScreenPoint(Input.mousePosition); //get position of cursor in world camera
+                                                                                                                                        //angleToCursor = Mathf.Atan2(cursorPosition.y, cursorPosition.x) * Mathf.Rad2Deg; //calculate angle to cursor to rotate bulletSpawner
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, cursorPosition - transform.position);
 
-
-        bulletSpawner.transform.rotation = spawnerRotation * Quaternion.Euler(0, 0, angleToCursor); //rotation of bulletspawner around player to the cursor
+        /*
+        rotation = cursorPosition - transform.position;
+        rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rotZ);
+        */
+        //transform.rotation = transform.rotation * Quaternion.Euler(0, 0, rotZ); //rotation of bulletspawner around player to the cursor
+        if(Input.GetButtonUp("Fire1"))
+        {
+        }
     }
 
-    void Shooting()
-    {
 
-    }
 }
